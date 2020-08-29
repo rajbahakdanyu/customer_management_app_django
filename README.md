4611342b4bfb2806a18e43a58df53ae5785349a8
Danyu Rajbahak <35065226+rajbahakdanyu@users.noreply.github.com>
2020-08-29 16:42:32 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# customer_management_app_django
---
+++ accounts/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+
+def home(request):
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders':orders, 'customers':customers, 
+    'total_orders':total_orders,'delivered':delivered,'pending':pending}
+
+    return render(request, 'accounts/dashboard.html', context)
+
+def products(request):
+    products = Products.objects.all()
+    return render(request, 'accounts/products.html', {'products':products})
+
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+
+    total_orders = orders.count()
+
+    context = {'customer':customer, 'orders':orders, 'orders_count':total_orders}
+
+    return render(request, 'accounts/customer.html', context)
+
+def createOrder(request):
+    context = {}
+
+    return render(request, 'accounts/order_form.html', context)
